64c04c3c10e0cec2edbfee244240ca4ab7e6ddff
Mac <mac@Mac-MacBook-Pro.local>
2025-12-30 00:11:58 +0300

    style: Increase settings icon size to 20x20

--- packages/charts/src/gui/attribute_bar/components/Icons.ts
+++ packages/charts/src/gui/attribute_bar/components/Icons.ts
@@ -20,7 +20,7 @@ export const ICONS = {
         <line x1="3" y1="18" x2="21" y2="18"/>
     </svg>`,
 
-    settings: `<svg width="16" height="16" viewBox="0 0 28 28" fill="currentColor">
+    settings: `<svg width="20" height="20" viewBox="0 0 28 28" fill="currentColor">
         <path fill-rule="evenodd" d="M18 14a4 4 0 1 1-8 0 4 4 0 0 1 8 0Zm-1 0a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
         <path fill-rule="evenodd" d="M8.5 5h11l5 9-5 9h-11l-5-9 5-9Zm-3.86 9L9.1 6h9.82l4.45 8-4.45 8H9.1l-4.45-8Z"/>
     </svg>`,
