cc4b628d4c00f1070dfca039997ea5e64dac5fc5
Mac <mac@Mac-MacBook-Pro.local>
2025-12-30 00:21:47 +0300

    style: Update all icons to TradingView style (25x25 size)
    - Settings icon: hexagon gear
    - Lock icon: filled padlock style
    - Delete icon: filled trash style
    - Clone icon: updated
    - Increased icon sizes for better visibility

--- packages/charts/src/gui/attribute_bar/components/Icons.ts
+++ packages/charts/src/gui/attribute_bar/components/Icons.ts
@@ -20,25 +20,20 @@ export const ICONS = {
         <line x1="3" y1="18" x2="21" y2="18"/>
     </svg>`,
 
-    settings: `<svg width="20" height="20" viewBox="0 0 28 28" fill="currentColor">
+    settings: `<svg width="25" height="25" viewBox="0 0 28 28" fill="currentColor">
         <path fill-rule="evenodd" d="M18 14a4 4 0 1 1-8 0 4 4 0 0 1 8 0Zm-1 0a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
         <path fill-rule="evenodd" d="M8.5 5h11l5 9-5 9h-11l-5-9 5-9Zm-3.86 9L9.1 6h9.82l4.45 8-4.45 8H9.1l-4.45-8Z"/>
     </svg>`,
 
-    lock: `<svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-        <rect x="3" y="11" width="18" height="11" rx="2" ry="2"/>
-        <path d="M7 11V7a5 5 0 0 1 10 0v4"/>
+    lock: `<svg width="25" height="25" viewBox="0 0 28 28" fill="currentColor">
+        <path fill-rule="evenodd" d="M14 6a3 3 0 0 0-3 3v3h8.5a2.5 2.5 0 0 1 2.5 2.5v7a2.5 2.5 0 0 1-2.5 2.5h-11A2.5 2.5 0 0 1 6 21.5v-7A2.5 2.5 0 0 1 8.5 12H10V9a4 4 0 0 1 8 0h-1a3 3 0 0 0-3-3zm-1 11a1 1 0 1 1 2 0v2a1 1 0 1 1-2 0v-2zm-6-2.5c0-.83.67-1.5 1.5-1.5h11c.83 0 1.5.67 1.5 1.5v7c0 .83-.67 1.5-1.5 1.5h-11A1.5 1.5 0 0 1 7 21.5v-7z"/>
     </svg>`,
 
-    delete: `<svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-        <polyline points="3 6 5 6 21 6"/>
-        <path d="M19 6l-2 14H7L5 6"/>
-        <path d="M10 11v6"/>
-        <path d="M14 11v6"/>
-        <path d="M9 6V4h6v2"/>
+    delete: `<svg width="25" height="25" viewBox="0 0 28 28" fill="currentColor">
+        <path fill-rule="evenodd" d="M18 7h5v1h-2.01l-1.33 14.64a1.5 1.5 0 0 1-1.5 1.36H9.84a1.5 1.5 0 0 1-1.49-1.36L7.01 8H5V7h5V6c0-1.1.9-2 2-2h4a2 2 0 0 1 2 2v1Zm-6-2a1 1 0 0 0-1 1v1h6V6a1 1 0 0 0-1-1h-4ZM8.02 8l1.32 14.54a.5.5 0 0 0 .5.46h8.33a.5.5 0 0 0 .5-.46L19.99 8H8.02Z"/>
     </svg>`,
 
-    clone: `<svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+    clone: `<svg width="25" height="25" viewBox="0 0 28 28" fill="currentColor">
         <rect x="9" y="9" width="13" height="13" rx="2" ry="2"/>
         <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"/>
     </svg>`,
